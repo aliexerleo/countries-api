797ea4c364cf0d1ffc4266691e5918adb0195de8
aliexerleo <aliexermayor@hotmail.com>
2021-12-23 21:26:20 -0500

    method delete completed

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from apps.country.models import Country
 
+
 # Serializers
 from api.serializers import CountrySerializer
 
@@ -12,19 +13,24 @@ class CountryViewSet(ViewSet):
         serializer = Country.objects.all()
         serializer = CountrySerializer(serializer, many=True)
         return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-    def retrieve(self, request, pk: int):
-        only_country = CountrySerializer(Country.objects.get(pk=pk))
-        return Response(status=status.HTTP_200_OK, data=only_country.data)
-
+    
     def create(self, request):
         serializer = CountrySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(status=status.HTTP_201_CREATED, data=serializer.data)
 
-    def destroy(self, request, pk: int):
-        only_country = CountrySerializer(Country.objects.get(pk=pk))
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def retrieve(self, request, pk: int):
+        instance = CountrySerializer(Country.objects.get(pk=pk))
+        return Response(status=status.HTTP_200_OK, data=instance.data)
 
-        
+    def update(self, request, pk: int):
+        pass
+
+    def destroy(self, request, pk: int):
+        instance = Country.objects.get(pk=pk)
+        instance.delete()
+        messages= {
+            'messages': 'country was deleted'
+        }
+        return Response(status=status.HTTP_204_NO_CONTENT, data=messages)
